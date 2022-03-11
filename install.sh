1cbec6370e2e0583650427a9eed5ea0f4c81b1c4
Gitznik <r.offner@gmx.de>
2022-03-11 17:19:16 +0100

    Add all files to install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 echo "Linking Files..."
 
 [[ -s "$HOME/.zshrc" ]] && rm ~/.zshrc
+[[ -s "$HOME/.zsh_aliases" ]] && rm ~/.zsh_aliases
+[[ -s "$HOME/Library/Application\ Support/Code/User/settings.json" ]] && rm ~/Library/Application\ Support/Code/User/settings.json
 
 ln ./.zshrc ~/.zshrc
+ln ./.zsh_aliases ~/.zsh_aliases
+ln ./vscode_settings.json ~/Library/Application\ Support/Code/User/settings.json
 
 echo "Done"
