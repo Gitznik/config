b11a78288143005af9e8e9d70e7a9ec49fce4fa5
Gitznik <r.offner@gmx.de>
2022-03-17 14:52:14 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 echo "Linking Files..."
 
+[[ -s "$HOME/.zprofile" ]] && rm ~/.zprofile
 [[ -s "$HOME/.zshrc" ]] && rm ~/.zshrc
 [[ -s "$HOME/.zsh_aliases" ]] && rm ~/.zsh_aliases
 [[ -s "$HOME/Library/Application\ Support/Code/User/settings.json" ]] && rm ~/Library/Application\ Support/Code/User/settings.json
 
+ln ./.zprofile ~/.zprofile
 ln ./.zshrc ~/.zshrc
 ln ./.zsh_aliases ~/.zsh_aliases
 ln ./vscode_settings.json ~/Library/Application\ Support/Code/User/settings.json
